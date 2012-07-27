e8034344a16227bc6d8a73ab79300ca7386e1853
Gerald Musser <gerald@musser.ws>
2012-07-27 13:56:17 +0200

    git test commit

--- Paintroid/Paintroid/src/at/tugraz/ist/paintroid/MainActivity.java
+++ Paintroid/Paintroid/src/at/tugraz/ist/paintroid/MainActivity.java
@@ -23,7 +23,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+//git test comitt
 package at.tugraz.ist.paintroid;
 
 import java.io.File;

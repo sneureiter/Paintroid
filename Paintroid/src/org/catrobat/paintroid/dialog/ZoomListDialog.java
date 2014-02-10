a39f13f3da922dc717c49f3b4a7f5ddec3d733f4
Roland Urbano <urbano@student.tugraz.at>
2014-02-10 12:52:26 +0100

    added List to spinner added OnClickListener

--- Paintroid/src/org/catrobat/paintroid/dialog/ZoomListDialog.java
+++ Paintroid/src/org/catrobat/paintroid/dialog/ZoomListDialog.java
@@ -1,22 +1,23 @@
 package org.catrobat.paintroid.dialog;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.catrobat.paintroid.MainActivity;
+import org.catrobat.paintroid.PaintroidApplication;
 import org.catrobat.paintroid.R;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
 
-public class ZoomListDialog extends BaseDialog {
+public class ZoomListDialog extends BaseDialog implements
+		android.view.View.OnClickListener {
 
 	public static ZoomListDialog instance;
 
 	private static final String NOT_INITIALIZED_ERROR_MESSAGE = "ZoomListDialog has not been initialized. Call init() first!";
 	private MainActivity mParent;
+	Spinner zoomSelector;
 
 	public ZoomListDialog(Context context) {
 		super(context);
@@ -41,15 +42,76 @@ public class ZoomListDialog extends BaseDialog {
 		setTitle(R.string.dialog_tools_title); // change to new string
 		setCanceledOnTouchOutside(true);
 
-		Spinner zoomSelector = (Spinner) findViewById(R.id.spinner1);
-		List<String> list = new ArrayList<String>();
-		list.add("list 1");
-		list.add("list 2");
-		list.add("list 3");
-		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(mParent,
-				android.R.layout.simple_spinner_item, list);
-		dataAdapter
-				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		zoomSelector.setAdapter(dataAdapter);
+		zoomSelector = (Spinner) findViewById(R.id.spinner1);
+		zoomSelector.setSelection(5);
+		Button submitBtn = (Button) findViewById(R.id.acceptBtn);
+		submitBtn.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		int itemPosition = zoomSelector.getSelectedItemPosition();
+		switch (itemPosition) {
+		case 0:
+			PaintroidApplication.perspective.setScale(5f);
+			dismiss();
+			break;
+		case 1:
+			PaintroidApplication.perspective.setScale(4f);
+			dismiss();
+			break;
+		case 2:
+			PaintroidApplication.perspective.setScale(3f);
+			dismiss();
+			break;
+		case 3:
+			PaintroidApplication.perspective.setScale(2f);
+			dismiss();
+			break;
+		case 4:
+			PaintroidApplication.perspective.setScale(1.5f);
+			break;
+		case 5:
+			PaintroidApplication.perspective.setScale(0.95f);
+			dismiss();
+			break;
+		case 6:
+			PaintroidApplication.perspective.setScale(0.855f);
+			dismiss();
+			break;
+		case 7:
+			PaintroidApplication.perspective.setScale(0.76f);
+			dismiss();
+			break;
+		case 8:
+			PaintroidApplication.perspective.setScale(0.665f);
+			dismiss();
+			break;
+		case 9:
+			PaintroidApplication.perspective.setScale(0.57f);
+			dismiss();
+			break;
+		case 10:
+			PaintroidApplication.perspective.setScale(0.475f);
+			dismiss();
+			break;
+		case 11:
+			PaintroidApplication.perspective.setScale(0.38f);
+			dismiss();
+			break;
+		case 12:
+			PaintroidApplication.perspective.setScale(0.285f);
+			dismiss();
+			break;
+		case 13:
+			PaintroidApplication.perspective.setScale(0.19f);
+			dismiss();
+			break;
+		case 14:
+			PaintroidApplication.perspective.setScale(0.1f);
+			dismiss();
+			break;
+		}
+
 	}
 }
